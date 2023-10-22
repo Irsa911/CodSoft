72ec8406178536dd7a7095d9f82da19f5dfb3aac
Irsa911 <140706602+Irsa911@users.noreply.github.com>
2023-10-22 18:25:49 +0500

    Update script.js

--- Calculator/script.js
+++ Calculator/script.js
@@ -23,25 +23,3 @@ Array.from(button).forEach((button) => {
 })
 });
       
-// let string = "";
-// let buttons = document.querySelectorAll(".button");
-
-// Array.from(buttons).forEach((button) => {
-//     button.addEventListener("click", (e) => {
-//         if (e.target.innerHTML === "=") {
-//             try {
-//                 string = eval(string);
-//                 document.querySelector("input").value = string;
-//             } catch (error) {
-//                 document.querySelector("input").value = "Error";
-//             }
-//         } else if (e.target.innerHTML === "C") {
-//             string = "";
-//             document.querySelector("input").value = string;
-//         } else {
-//             string += e.target.innerHTML;
-//             document.querySelector("input").value = string;
-//         }
-//     });
-// });
-
